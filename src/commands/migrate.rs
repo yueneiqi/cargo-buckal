45e3b2184342c5bd4256214bf1ae8c6bd40a6f0d
linfeng.ouyang <fyoylf@gmail.com>
2025-12-22 13:16:04 +0800

    refactor: move platforms and toolchains to buckal from bundles
    Signed-off-by: linfeng.ouyang <fyoylf@gmail.com>

--- src/commands/migrate.rs
+++ src/commands/migrate.rs
@@ -6,7 +6,7 @@ use crate::{
     RUST_CRATES_ROOT,
     buck2::Buck2Command,
     buckify::flush_root,
-    bundles::{fetch_buckal_cell, init_buckal_cell, init_modifier},
+    bundles::{fetch_buckal_cell, init_buckal_cell, init_modifier, init_toolchains_buck},
     cache::BuckalCache,
     context::BuckalContext,
     utils::{UnwrapOrExit, check_buck2_package, ensure_prerequisites},
@@ -71,6 +71,9 @@ pub fn execute(args: &MigrateArgs) {
         // Configure the buckal cell in .buckconfig
         let cwd = std::env::current_dir().unwrap_or_exit();
         init_buckal_cell(&cwd).unwrap_or_exit();
+
+        // Write toolchains/BUCK to use buckal demo toolchains
+        init_toolchains_buck(&cwd).unwrap_or_exit();
 
         // Init cfg modifiers
         init_modifier(&cwd).unwrap_or_exit();
